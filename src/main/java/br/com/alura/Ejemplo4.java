4327ac293fac2fec349ae5cb361b4329af8e88c0
Milton Figueredo Miles <gratno@gmail.com>
2023-01-21 14:24:41 -0500

    Create list whit object

---
+++ src/main/java/br/com/alura/Ejemplo4.java
@@ -0,0 +1,23 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class Ejemplo4 {
+
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso("Geometría", 30);
+        Curso curso2 = new Curso("Física", 30);
+        Curso curso3 = new Curso("Química", 30);
+        Curso curso4 = new Curso("Historia", 30);
+
+        ArrayList<String> cursos = new ArrayList<>();
+
+    }
+}
