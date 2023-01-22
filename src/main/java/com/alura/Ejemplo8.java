747284cf216e6a2d0319a411663f8e5cd1d6114c
Milton Figueredo Miles <gratno@gmail.com>
2023-01-22 11:35:04 -0500

    ArrayList and LinkedList

--- src/main/java/com/alura/Ejemplo8.java
+++ src/main/java/com/alura/Ejemplo8.java
@@ -1,12 +1,41 @@
 package com.alura;
 
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ejemplo8 {
+
+    public static final List<List<Integer>> listas = new ArrayList<List<Integer>>(){
+        {
+            add(new LinkedList<>());
+            add(new ArrayList<>());
+        }
+    };
 
     public static void main(String[] args) {
 
+        for (List<Integer> lista : listas) {
+            final String nombreImplementacion = lista.getClass().getSimpleName();
+
+       //add
+        long inicio = System.currentTimeMillis();
+        for (int i = 0 ; i < 10000000 ; i++) {
+            lista.add(i);
+        }
+        long fin = System.currentTimeMillis();
+        long duracion = fin - inicio;
+        System.out.println(nombreImplementacion + " add: " + duracion);
+
+        //get
+        inicio = System.currentTimeMillis();
 
 
-        System.out.println("holas");
+        //rem,ove
+
+        }
+
+        //System.out.println(final);
 
     }
 }
