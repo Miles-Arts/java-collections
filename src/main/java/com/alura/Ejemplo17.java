9555f37e7dca8e460fd680091ec7293e03b1044f
Milton Figueredo Miles <gratno@gmail.com>
2023-01-23 16:59:14 -0500

    Maps - optional -

---
+++ src/main/java/com/alura/Ejemplo17.java
@@ -0,0 +1,60 @@
+package com.alura;
+
+import com.alura.model.Alumno;
+import model.Curso;
+
+import java.util.Iterator;
+
+public class Ejemplo17 {
+
+    public static void main(String[] args) {
+
+        //System.out.println("----");
+
+        Curso curso1 = new Curso("Historia", 30);
+
+        Alumno alumno1 = new Alumno("Luis Miguel",  "001");
+        Alumno alumno2 = new Alumno("Pepito Juan Palotes",  "002");
+        Alumno alumno3 = new Alumno("Juan Carlos",  "003");
+        Alumno alumno4 = new Alumno("Pedro Pink",  "004");
+        Alumno alumno5 = new Alumno("Gustavo Sanchez",  "005");
+        Alumno alumno6 = new Alumno("Marcia María",  "006");
+        Alumno alumno7 = new Alumno("Claudia María",  "007");
+
+       //Collection<Alumno> listaAlumnos = new HashSet<>();
+        //Collection<Alumno> listaAlumnos = new ArrayList<>();
+
+        curso1.addAlumno(alumno1);
+        curso1.addAlumno(alumno2);
+        curso1.addAlumno(alumno3);
+        curso1.addAlumno(alumno4);
+        curso1.addAlumno(alumno5);
+        curso1.addAlumno(alumno6);
+        curso1.addAlumno(alumno7);
+
+        System.out.println("----");
+
+        //curso1.getAlumnos().forEach(alumno ->  System.out.println(alumno));
+
+       // curso1.getAlumnos().forEach(alumno ->  System.out.println(alumno));
+
+
+        System.out.println("----");
+
+        Iterator<Alumno> alumnoIterator = curso1.getAlumnos().iterator();
+
+           while (alumnoIterator.hasNext()){
+               System.out.println(alumnoIterator.next());
+           };
+
+       /* try {
+            alumnoIterator.next();
+        } catch (NoSuchElementException e){
+            System.out.println("No Existe");
+        }*/
+
+        System.out.println("----");
+
+    }
+
+}
